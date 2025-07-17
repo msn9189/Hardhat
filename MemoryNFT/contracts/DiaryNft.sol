a0b5fb3035c67c87242509718084234b4c9dbba5
Nazi <mehdizadeh.sn@gmail.com>
2025-07-17 21:40:47 +0000

    DiaryNFT file modified to remove Counters.sol from the contract

--- MemoryNFT/contracts/DiaryNft.sol
+++ MemoryNFT/contracts/DiaryNft.sol
@@ -3,16 +3,13 @@ pragma solidity ^0.8.20;
 
 // Import Openzeppelin's ERC721 contract for standard NFT functionality.
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-// Imort OpenZeppelin's Counters utility for safe, incremental token Id management.
-import "@openzeppelin/contracts/utils/Counters.sol";
 
 // Define th DiaryNFT contract, inheriting from ERC721 to create NFTs.
 contract DiaryNFT is ERC721 {
 
-    // Enable Counters library functions for the Counters.counter type.
-    using Counters for Counters.Counter;
-    // Private counter to track the next availabe token ID for minting.
-    Counters.Counter private _tokenIds;
+    // Private counter to track the next available token ID for minting.
+    uint256 private _tokenIds;
+
     // Private mapping to store IPFS hashes (metadata URIs) for each tokenID.
     mapping(uint256 => string) private _tokenURIs;
 
@@ -20,17 +17,19 @@ contract DiaryNFT is ERC721 {
     event DiaryMinted(uint256 indexed tokenId, address indexed recipient, string ipfsHash);
 
     // Constructor initializes the ERC721 contract with name "DiaryNFT" and symbol "DIARY'
-    constructor() ERC721("DiaryNFT", "DIARY"){}
+    constructor() ERC721("DiaryNFT", "DIARY"){
+        _tokenIds = 0; // Initialize the token ID counter to 0;
+    }
 
     // Mints a new NFT to the recipient address with the provided IPFS hash as metadata.
     // Public function, anyone can call it. Returns the new token ID.
     function mintDiary(address recipient, string memory ipfsHash) public returns (uint256){
-        // Increment the token ID counter to generate an unique ID.
-        _tokenIds.increment();
+        // Increment the token ID counter;
+        _tokenIds++;
         // Get the current (newly incremented) token ID.
-        uint256 newTokenId = _tokenIds.current();
+        uint256 newTokenId = _tokenIds;
         // Mint the NFT to the recipient with the new token ID, ensuring safe transfer.
-        _safemint(recipient, newTokenId);
+        _safeMint(recipient, newTokenId);
         // Store the IPFS hash in the tokenURIs mapping for the new token ID.
         _tokenURIs[newTokenId] = ipfsHash;
         // Emit the DiaryMinted event to log.
