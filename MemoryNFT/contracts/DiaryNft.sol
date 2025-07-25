b9ca4cb81aeb788763c91e57688c490c1386ac5d
Nazi <mehdizadeh.sn@gmail.com>
2025-07-25 09:09:57 +0000

    tokenId variable changed to a public variable

--- MemoryNFT/contracts/DiaryNft.sol
+++ MemoryNFT/contracts/DiaryNft.sol
@@ -5,10 +5,12 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 // Define th DiaryNFT contract, inheriting from ERC721 to create NFTs.
+import "hardhat/console.sol";
+
 contract DiaryNFT is ERC721 {
 
     // Private counter to track the next available token ID for minting.
-    uint256 private _tokenIds;
+    uint256 public tokenIds;
 
     // Private mapping to store IPFS hashes (metadata URIs) for each tokenID.
     mapping(uint256 => string) private _tokenURIs;
@@ -18,22 +20,30 @@ contract DiaryNFT is ERC721 {
 
     // Constructor initializes the ERC721 contract with name "DiaryNFT" and symbol "DIARY'
     constructor() ERC721("DiaryNFT", "DIARY"){
-        _tokenIds = 0; // Initialize the token ID counter to 0;
+        tokenIds = 0; // Initialize the token ID counter to 0;
     }
 
     // Mints a new NFT to the recipient address with the provided IPFS hash as metadata.
     // Public function, anyone can call it. Returns the new token ID.
     function mintDiary(address recipient, string memory ipfsHash) public returns (uint256){
         // Increment the token ID counter;
-        _tokenIds++;
+        tokenIds++;
         // Get the current (newly incremented) token ID.
-        uint256 newTokenId = _tokenIds;
+        uint256 newTokenId = tokenIds;
         // Mint the NFT to the recipient with the new token ID, ensuring safe transfer.
         _safeMint(recipient, newTokenId);
+
+        console.log(
+            "The new token Id is %s .",
+            newTokenId
+        );
+        
         // Store the IPFS hash in the tokenURIs mapping for the new token ID.
         _tokenURIs[newTokenId] = ipfsHash;
         // Emit the DiaryMinted event to log.
         emit DiaryMinted(newTokenId, recipient, ipfsHash);
+
+       
         // Return the new token ID to the caller.
         return newTokenId;
     }
