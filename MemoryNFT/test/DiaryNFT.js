46722cfa909de61d41830130c16b0282bb3fac18
Nazi <mehdizadeh.sn@gmail.com>
2025-07-26 04:31:00 +0000

    writing tests for the contract

--- MemoryNFT/test/DiaryNFT.js
+++ MemoryNFT/test/DiaryNFT.js
@@ -1,18 +1,20 @@
 // Import Chai's expect function for assertion in tests.
 const { expect } = require("chai");
 // Import Hardhat's ethers library to interact with the Ethereum blockchain and contracts.
-const { ethers } = require("hardhat"); 
+const { ethers } = require("hardhat");
 
-const { loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+const {
+  loadFixture,
+} = require("@nomicfoundation/hardhat-toolbox/network-helpers");
 
 // Describe block defines a test suite for the DiaryNFT contract.
 describe("DiaryNFT", function () {
   async function deployDiaryNFTFixture() {
     const [owner, addr1] = await ethers.getSigners();
 
-     const diaryNFT = await ethers.deployContract("DiaryNFT");
+    const diaryNFT = await ethers.deployContract("DiaryNFT");
 
-     await diaryNFT.waitForDeployment();
+    await diaryNFT.waitForDeployment();
 
     return { diaryNFT, owner, addr1 };
   }
@@ -22,9 +24,23 @@ describe("DiaryNFT", function () {
       const { diaryNFT, owner, addr1 } = await loadFixture(
         deployDiaryNFTFixture
       );
-
       expect(await diaryNFT.tokenIds()).to.equal(0);
-      
+
+      const ipfsHash = "QmTestHash123";
+
+      const tx = await diaryNFT.mintDiary(addr1.address, ipfsHash);
+      const Id = await diaryNFT.tokenIds();
+
+      expect(Id).to.equal(1);
+
+      expect(await diaryNFT.ownerOf(Id)).to.equal(addr1.address);
+      expect(await diaryNFT.tokenURI(Id)).to.equal(ipfsHash);
+
+      expectEvent.inLogs(logs, "DiaryMinted", {
+        tokenId: Id,
+        recipient: addr1.address,
+        ipfsHash: ipfsHash,
+      });
     });
 
     it("Fails for nonexistent token URI", async function () {
