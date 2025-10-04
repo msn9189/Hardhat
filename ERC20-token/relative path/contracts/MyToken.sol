0061432693b61b50ceb792ff04f27079945f534d
Nazi <mehdizadeh.sn@gmail.com>
2025-10-04 13:36:22 +0000

    constructor function added to the token

--- ERC20-token/relative path/contracts/MyToken.sol
+++ ERC20-token/relative path/contracts/MyToken.sol
@@ -5,5 +5,11 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyToken is ERC20, Ownable {
-    
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        uint256 initialSupply
+        ) ERC20(name_, symbol_) Ownable(msg.sender){
+        _mint(msg.sender, initialSupply * 10 ** decimals());
+    }
 }
