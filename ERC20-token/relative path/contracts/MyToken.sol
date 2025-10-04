c1a2e51aeef104cec2a2d5187ce3d9a86979c1ae
Nazi <mehdizadeh.sn@gmail.com>
2025-10-04 13:38:44 +0000

    mint function added

--- ERC20-token/relative path/contracts/MyToken.sol
+++ ERC20-token/relative path/contracts/MyToken.sol
@@ -12,4 +12,8 @@ contract MyToken is ERC20, Ownable {
         ) ERC20(name_, symbol_) Ownable(msg.sender){
         _mint(msg.sender, initialSupply * 10 ** decimals());
     }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount * 10 ** decimals());
+    }
 }
