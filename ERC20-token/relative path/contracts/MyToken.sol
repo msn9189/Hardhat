68a8aec67e073fca15e4053cbd42a0cc3876fa15
Nazi <mehdizadeh.sn@gmail.com>
2025-10-04 14:26:47 +0000

    comments added to the file

--- ERC20-token/relative path/contracts/MyToken.sol
+++ ERC20-token/relative path/contracts/MyToken.sol
@@ -4,7 +4,16 @@ pragma solidity ^0.8.18;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+/// @title MyToken - Simplified ERC20 token for learning
+/// @author nazii.eth
+/// @notice Includes mint and burn functions
+/// @dev Based on Openzeppelin's ERC20 and Ownable contracts
+
 contract MyToken is ERC20, Ownable {
+    /// @notice Deploy token with name, symbol and initial supply to the owner
+    /// @param name_ The token name (e.g, "MyToken") 
+    /// @param symbol_  The token symbol (e.g., "MTK")
+    /// @param initialSupply Initial token supply (without decimals)
     constructor(
         string memory name_,
         string memory symbol_,
@@ -13,10 +22,16 @@ contract MyToken is ERC20, Ownable {
         _mint(msg.sender, initialSupply * 10 ** decimals());
     }
 
+    /// @notice Mint new tokens to an address
+    /// @dev Only owner can call this function
+    /// @param to Recipient address
+    /// @param amount Amount of tokens to mint (without decimals)
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount * 10 ** decimals());
     }
 
+    /// @notice Burn tokens from an address
+    /// @param amount Amount of tokens to burn (without decimals)
     function burn(uint256 amount) public onlyOwner {
         _burn(msg.sender, amount * 10 ** decimals());
     }
