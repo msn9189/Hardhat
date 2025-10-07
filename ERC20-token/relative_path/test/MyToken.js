6465223905983e6eadc02036405dbe3d8aac7456
Nazi <mehdizadeh.sn@gmail.com>
2025-10-07 03:16:28 +0000

    Test file edited but faces error yet

--- ERC20-token/relative_path/test/MyToken.js
+++ ERC20-token/relative_path/test/MyToken.js
@@ -1,28 +1,36 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
-
-const { loadFixture } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 
 describe("MyToken", function () {
-
   async function deployMyTokenFixture() {
     const [owner, alice, bob, carol] = await ethers.getSigners();
     const initialSupply = "1000";
-    const MyToken = await ethers.deployContract("MyToken",["MyToken", "MTK", initialSupply]);
-    await MyToken.waitForDeployment();
+
+    let token;
+    try {
+      token = await ethers.deployContract("MyToken", [
+      "MyToken",
+      "MTK",
+      initialSupply,
+    ]);
+    await token.waitForDeployment();
+  } catch {
+    const Factory = await ethers.getContractFactory("MyTokne");
+    token = await Factory.deploy("MyToken", "MTK", initialSupply);
+    await token.deployed();
+  } 
+    
     return { MyToken, owner, alice, bob, carol, initialSupply };
   }
 
   describe("Deployment", function () {
     it("Has correct name, symbol and decimals", async function () {
-      const { MyToken } = await loadFixture(
-        deployMyTokenFixture
-      );
+      const { MyToken } = await loadFixture(deployMyTokenFixture);
 
       expect(await MyToken.name()).to.equal("MyToken");
       expect(await MyToken.symbol()).to.equal("MTK");
       expect(await MyToken.decimals()).to.equal(18);
-
     });
   });
 });
