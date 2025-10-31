b37af7c288400c6d7e43d5602178efa564512c00
Nazi <mehdizadeh.sn@gmail.com>
2025-10-31 07:22:03 +0000

    loadfixture defined in the test file

--- MyToken/absolute/test/Counter.ts
+++ MyToken/absolute/test/Counter.ts
@@ -1,33 +1,12 @@
 import { expect } from "chai";
-import { network } from "hardhat";
-
-const { ethers } = await network.connect();
+import ethers from "hardhat";
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 
 describe("Counter", function () {
-  it("deploys with initial value and increments/decrements with events", async function () {
+  async function deployCounterFixture() {
     const [signer] = await ethers.getSigners();
-
     const counter = await ethers.deployContract("Counter", [5n]);
     await counter.waitForDeployment();
-
-    expect(await counter.count()).to.equal(5n);
-
-    await expect(counter.increment())
-      .to.emit(counter, "Incremented")
-      .withArgs(signer.address, 6n);
-
-    expect(await counter.count()).to.equal(6n);
-
-    await expect(counter.decrement())
-      .to.emit(counter, "Decremented")
-      .withArgs(signer.address, 5n);
-
-    expect(await counter.count()).to.equal(5n);
-  });
-
-  it("reverts on decrement when count is zero", async function () {
-    const counter = await ethers.deployContract("Counter", [0n]);
-    await counter.waitForDeployment();
-    await expect(counter.decrement()).to.be.revertedWith("Underflow");
-  });
-});
+    return { counter, signer };
+  }
+})
