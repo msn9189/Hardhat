5e67bdc19029ba11c23ff564515864357135132e
Nazi <mehdizadeh.sn@gmail.com>
2025-11-01 20:22:11 +0000

    comment added to describe the file

--- MyToken/absolute/test/sanity.cjs
+++ MyToken/absolute/test/sanity.cjs
@@ -1,3 +1,12 @@
+/**
+ * @title Sanity Check Test (CommonJS)
+ * @notice Verifies that the testing environment is set up and runs correctly.
+ * @dev This is a simple test to confirm that Mocha/Chai and Hardhat configurations work without errors.
+ */
 describe("sanity-cjs", function () {
+  /**
+   * @notice Verifies that the testing environment is set up and runs correctly.
+   * @dev This is a simple test to confirm that Mocha/Chai and Hardhat configurations work without errors.
+   */
   it("runs", function () {});
 });
