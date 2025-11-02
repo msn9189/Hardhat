fe40c032278d19355d4db83366d4149a4c0c5785
Nazi <mehdizadeh.sn@gmail.com>
2025-11-02 09:15:18 +0000

    License Identifir changed to MIT

--- simple-bank/absolute/contracts/SimpleBank.sol
+++ simple-bank/absolute/contracts/SimpleBank.sol
@@ -1,19 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-contract Counter {
-  uint public x;
 
-  event Increment(uint by);
-
-  function inc() public {
-    x++;
-    emit Increment(1);
-  }
-
-  function incBy(uint by) public {
-    require(by > 0, "incBy: increment should be positive");
-    x += by;
-    emit Increment(by);
-  }
-}
