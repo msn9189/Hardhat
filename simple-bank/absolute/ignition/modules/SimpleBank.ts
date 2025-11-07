707ec718bbb0b52fa8b0b755374277109829b875
Nazi <mehdizadeh.sn@gmail.com>
2025-11-07 12:46:59 +0000

    a typo fixed

--- simple-bank/absolute/ignition/modules/SimpleBank.ts
+++ simple-bank/absolute/ignition/modules/SimpleBank.ts
@@ -1,7 +1,7 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 export default buildModule("SimpleBank", (m) => {
-  const SimpleBank = m.contract("ُSimpleBank");
+  const SimpleBank = m.contract("SimpleBank");
 
   return { SimpleBank };
 });
