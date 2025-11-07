42453f6edba7c667c5774e67820fde0354eee450
Nazi <mehdizadeh.sn@gmail.com>
2025-11-07 07:06:35 +0000

    Changed the file to the default setting

--- simple-bank/absolute/hardhat.config.ts
+++ simple-bank/absolute/hardhat.config.ts
@@ -1,71 +1,42 @@
-require("@nomicfoundation/hardhat-toolbox");
-/** @type import('hardhat/config').HardhatUserConfig */
-require("@nomicfoundation/hardhat-verify");
+import type { HardhatUserConfig } from "hardhat/config";
 
-const { vars } = require("hardhat/config");
-const { base } = require("viem/chains");
+import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
+import { configVariable } from "hardhat/config";
 
-const INFURA_API_KEY = vars.get("INFURA_API_KEY");
-
-const PRIVATE_KEY = vars.get("PRIVATE_KEY");
-const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
-
-module.exports = {
-  solidity: "0.8.28",
-  networks: {
-    sepolia: {
-      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [PRIVATE_KEY],
-    },
-    base: {
-      url: `https://base-mainnet.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [PRIVATE_KEY],
+const config: HardhatUserConfig = {
+  plugins: [hardhatToolboxMochaEthersPlugin],
+  solidity: {
+    profiles: {
+      default: {
+        version: "0.8.28",
+      },
+      production: {
+        version: "0.8.28",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
     },
   },
-  etherscan: {
-    apiKey: ETHERSCAN_API_KEY,
+  networks: {
+    hardhatMainnet: {
+      type: "edr-simulated",
+      chainType: "l1",
+    },
+    hardhatOp: {
+      type: "edr-simulated",
+      chainType: "op",
+    },
+    sepolia: {
+      type: "http",
+      chainType: "l1",
+      url: configVariable("SEPOLIA_RPC_URL"),
+      accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
+    },
   },
 };
 
-// import type { HardhatUserConfig } from "hardhat/config";
-
-// import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
-// import { configVariable } from "hardhat/config";
-
-// const config: HardhatUserConfig = {
-//   plugins: [hardhatToolboxMochaEthersPlugin],
-//   solidity: {
-//     profiles: {
-//       default: {
-//         version: "0.8.28",
-//       },
-//       production: {
-//         version: "0.8.28",
-//         settings: {
-//           optimizer: {
-//             enabled: true,
-//             runs: 200,
-//           },
-//         },
-//       },
-//     },
-//   },
-//   networks: {
-//     hardhatMainnet: {
-//       type: "edr-simulated",
-//       chainType: "l1",
-//     },
-//     hardhatOp: {
-//       type: "edr-simulated",
-//       chainType: "op",
-//     },
-//     sepolia: {
-//       type: "http",
-//       chainType: "l1",
-//       url: configVariable("SEPOLIA_RPC_URL"),
-//       accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
-//     },
-//   },
-// };
-
-// export default config;
+export default config;
