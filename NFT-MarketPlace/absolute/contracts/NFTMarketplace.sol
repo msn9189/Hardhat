a6ae8551d3e941b2e5901411c8ef8a3630894259
Nazi <mehdizadeh.sn@gmail.com>
2025-11-10 07:19:44 +0000

    a nested mapping defined for storing the listing item

--- NFT-MarketPlace/absolute/contracts/NFTMarketplace.sol
+++ NFT-MarketPlace/absolute/contracts/NFTMarketplace.sol
@@ -5,9 +5,12 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGaurd.sol";
 
 contract NFTMarketplace is ReentrancyGaurd {
-    
+
     struct Listing {
         address seller;
         uint256 price;
     }
+
+    mapping(address => mapping(uint256 => Listing)) public listing;
+    
 }
