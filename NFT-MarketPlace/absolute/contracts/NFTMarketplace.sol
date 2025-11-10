1bf510c0ff6b200431de9f284372bb0ad1e3843a
Nazi <mehdizadeh.sn@gmail.com>
2025-11-10 07:15:50 +0000

    necessary libraries imported

--- NFT-MarketPlace/absolute/contracts/NFTMarketplace.sol
+++ NFT-MarketPlace/absolute/contracts/NFTMarketplace.sol
@@ -1,2 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGaurd.sol";
