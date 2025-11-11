f67a920b29df33aca1b7018cea4cbd6b24670345
Nazi <mehdizadeh.sn@gmail.com>
2025-11-11 07:28:44 +0000

    the MockNFT contract completed

--- NFT-MarketPlace/absolute/contracts/MockNFT.sol
+++ NFT-MarketPlace/absolute/contracts/MockNFT.sol
@@ -3,3 +3,10 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
+contract MockNFT is ERC721 {
+    constructor() ERC721("MockNFT","MNFT"){}
+
+    function mint(address to, uint256 tokenId) external {
+        _mint(to, tokenId);
+    }
+}
