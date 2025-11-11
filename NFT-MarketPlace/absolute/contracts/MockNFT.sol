4a56908d5abcf77d81174f03af220914f3e58f78
Nazi <mehdizadeh.sn@gmail.com>
2025-11-11 07:30:56 +0000

    comments updated

--- NFT-MarketPlace/absolute/contracts/MockNFT.sol
+++ NFT-MarketPlace/absolute/contracts/MockNFT.sol
@@ -1,11 +1,20 @@
-// SPDX-License_Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
+/**
+ * @title MockNFT
+ * @dev Simple ERC721 token used for testing NFTMarketplace.
+ */
 contract MockNFT is ERC721 {
     constructor() ERC721("MockNFT","MNFT"){}
 
+    /**
+     * @notice Mint a new NFT to a specific address.
+     * @param to Address to receive th NFT.
+     * @param tokenId ID of the NFT to mint.
+     */
     function mint(address to, uint256 tokenId) external {
         _mint(to, tokenId);
     }
