9435cbfa89f4c5f1319de915122600fcacd0ee87
Nazi <mehdizadeh.sn@gmail.com>
2025-12-24 07:47:55 +0000

    safeTransferFrom function interface added

--- ERC1155/absolute/contracts/ERC1155.sol
+++ ERC1155/absolute/contracts/ERC1155.sol
@@ -2,5 +2,13 @@
 pragma solidity ^0.8.28;
 
 interface IERC1155 {
-  
+  function safeTransferFrom(
+        address from,
+        address to,
+        uint256 id,
+        uint256 value,
+        bytes calldata data
+    ) external;
+
+    
 }
