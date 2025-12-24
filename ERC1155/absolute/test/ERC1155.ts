983ee7dad30f1a42b4f105fc850fbd9ab18fa013
Nazi <mehdizadeh.sn@gmail.com>
2025-12-24 20:15:53 +0000

    variables defined

--- ERC1155/absolute/test/ERC1155.ts
+++ ERC1155/absolute/test/ERC1155.ts
@@ -3,6 +3,11 @@ import { network } from "hardhat";
 
 const { ethers } = await network.connect();
 
-describe("ERC1155", function (){
-  
+describe("ERC1155", function () {
+  let ERC1155: any;
+  let erc1155: Contract;
+  let owner: any;
+  let addr1: any;
+  let addr2: any;
+  let addr3: any;
 }) 
