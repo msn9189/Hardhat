94fa6620ba950ffa91a12f6d849bb3be5750bc94
Nazi <mehdizadeh.sn@gmail.com>
2026-06-28 07:54:43 +0000

    withdraw function to withdraw balance of address

--- simple-vault/contracts/simple-vault.sol
+++ simple-vault/contracts/simple-vault.sol
@@ -8,4 +8,11 @@ contract SimpleVault {
     balances[msg.sender] += amount;
   }
 
+  function withdraw(uint256 amount) public {
+    require(balances[msg.sender] >= amount, "Insufficient balance");
+    balances[msg.sender] -= amount;
+    payable(msg.sender).transfer(amount)
+  }
+
+  
 }
