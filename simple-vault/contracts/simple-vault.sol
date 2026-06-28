ce99b6ed3834624547e3dad7c569bad7e10e7eb4
Nazi <mehdizadeh.sn@gmail.com>
2026-06-28 07:51:23 +0000

    deposit function to store balances

--- simple-vault/contracts/simple-vault.sol
+++ simple-vault/contracts/simple-vault.sol
@@ -3,5 +3,9 @@ pragma solidity ^0.8.28;
 
 contract SimpleVault {
   mapping(address => uint256) public balances;
-  
+
+  function deposit(uint256 amount) public {
+    balances[msg.sender] += amount;
+  }
+
 }
