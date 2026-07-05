53ef6acea08b82d72e87e7a87d575c4509a465b6
Nazi <mehdizadeh.sn@gmail.com>
2026-07-05 20:02:57 +0000

    a block to test withdrawing more than the balance

--- simple-vault/test/Counter.ts
+++ simple-vault/test/Counter.ts
@@ -24,5 +24,10 @@ describe("SimpleVault", function () {
     const balance = await simpleVault.connect(user1).getBalance();
     expect(balance).to.equal(ethers.parseEther("1"))
   });
-
+  it("Should revert when withdrawing more than the balance", async function () {
+    const simpleVault = await ethers.deployContract("SimpleVault")
+    const [user1] = await ethers.getSigners()
+    await simpleVault.connect(user1).deposit({ value: ethers.parseEther("2")});
+    await expect(simpleVault.connect(user1).withdraw(ethers.parseEther("3"))).to.be.revertedWith("Insufficient balance");
+  })
 });
